ec582687645942ff66fb61225907dfcb59a21920
OldEase <tarasov.kd@phystech.edu>
2022-02-08 11:44:47 +0300

    Create 2_5.cpp

---
+++ 2 seminar/2_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+unsigned long long factor(int number){
+    unsigned long long result = 1;
+    if (number == 1){
+        return 1;
+    }
+    result = number*factor(number - 1);
+    return result;
+}
+
+int main(){
+    int number = 0;
+    std::cin >> number;
+    std::cout << factor(number);
+    return 0;
+}
